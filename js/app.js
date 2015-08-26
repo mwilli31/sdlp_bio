0e65ddb39fc5256a0a51fec859cf61ff8b4b8164
Michael Williams <mwilli31@nd.edu>
2015-08-26 18:24:54 -0400

    beginning work with interests and moved text to controller

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
 angular.module('app', ['ui.router'])
-.config(function($stateProvider) {
-  $stateProvider
-    .state('home', {
-      url: '',
-      templateUrl: '/templates/home.html',
-      controller: 'HomeController'
-    })
-    .state('home.profile', {
-      url:'',
-      templateUrl: '/templates/profile.html',
-      controller: 'ProfileController'
-    })
-})
-.controller('HomeController',
+.controller('MainController',
   function($scope) {
+    //Name, GE Business, Education, Location
+    $scope.name = "Michael Williams";
+    $scope.geBusiness1 = "GE Intelligent";  //only split on two lines if more than two words
+    $scope.geBusiness2 = "Platforms"        //leave this blank if "GE Software"
+    $scope.education1 = "University of";     //Same rules apply as above
+    $scope.education2 = "Notre Dame";
+    $scope.locationCityState = "Foxboro, MA"
+    $scope.locationCountry = "USA"
+
+    //Program Interests
+    $scope.interests = [];
+    $scope.interests.push("Web Development & UI Design for Industrial Big Data Analysis Products");
+    $scope.interests.push("Big Data Analyis and Machine Learning");
+    $scope.interests.push("Understand our industry partners' pain points and the vision of GE-IP");
+    $scope.interests.push("Apply Fastworks and Design Thinking processes on my projects to quickly build useful software for our customers");
+
+
+    //Skills
+    $scope.skills =         [{name:'JS',perc:'75%',goal:'95%'},
+                            {name:'HTML5',perc:'85%',goal:'98%'},
+                            {name:'UFT (QTP)',perc:'30%',goal:'80%'},
+                            {name:'Chef',perc:'10%',goal:'55%'},
+                            {name:'RSpec',perc:'10%',goal:'65%'}];
+
+    $scope.skillsDormant =  [{name:'C++',perc:'65%'},
+                            {name:'C',perc:'85%'},
+                            {name:'Objective-C',perc:'75%'}];
 })
-.controller('ProfileController',
-  function($scope) {
-});
